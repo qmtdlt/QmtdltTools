9496413d4916443647dff7e0bd9c1c097aa76249
qmtdlt <qmtdlt@gmail.com>
2025-02-22 17:07:56 +0800

    db operate

---
+++ api/src/QmtdltTools/QmtdltTools.EFCore/DC.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using QmtdltTools.Domain.Entitys;
+using Volo.Abp.Data;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace QmtdltTools.EFCore;
+
+[ConnectionStringName("Default")]
+public class DC:AbpDbContext<DC>
+{
+    private readonly IConfiguration _configuration;
+    public DC(DbContextOptions<DC> options,IConfiguration configuration):base(options)
+    {
+        _configuration = configuration;
+        this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configuration.GetConnectionString("Default");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 0));
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+    public DbSet<SysUser> SysUsers { get; set; }
+}
