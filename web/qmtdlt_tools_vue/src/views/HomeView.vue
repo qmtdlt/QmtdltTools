dcea638804693bc3b5eeccff1c107db380ee115f
qmtdlt <qmtdlt@gmail.com>
2025-02-26 18:08:35 +0800

    vue

---
+++ web/qmtdlt_tools_vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an home page</h1>
+    <el-button type="primary">Primary</el-button>
+  </div>
+</template>
---
+++ web/qmtdlt_tools_vue/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an login page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
