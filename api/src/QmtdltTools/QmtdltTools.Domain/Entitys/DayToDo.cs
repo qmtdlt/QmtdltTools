1af044eb0bc65d89b3ff8624ef62d25c153f323b
qmtdlt <qmtdlt@gmail.com>
2025-02-26 18:25:17 +0800

    db entity

---
+++ api/src/QmtdltTools/QmtdltTools.Domain/Entitys/DayToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QmtdltTools.Domain.Entitys
+{
+    [Table("DayToDo")]
+    public class DayToDo:EntityBaseId
+    {
+        public string Content { get; set; }
+        public DateTime FinishTime { get; set; }
+    }
+}
---
+++ api/src/QmtdltTools/QmtdltTools.Domain/Entitys/YearToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QmtdltTools.Domain.Entitys
+{
+    [Table("YearToDo")]
+    public class YearToDo:EntityBaseId
+    {
+        public string Content { get; set; }
+        public DateTime? FinishTime { get; set; }
+        public bool? IsReject { get; set; }
+    }
+}
