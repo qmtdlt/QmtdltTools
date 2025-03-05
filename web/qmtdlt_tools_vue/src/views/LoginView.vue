4755660fb1e874205ed1897a20de6b49ac1cde3e
qmtdlt <qmtdlt@gmail.com>
2025-03-05 14:00:59 +0800

    layout style modify

--- web/qmtdlt_tools_vue/src/views/LoginView.vue
+++ web/qmtdlt_tools_vue/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="login">
     <h1>This is an login page</h1>
   </div>
 </template>
@@ -9,4 +9,19 @@
 </script>
 
 <style scoped>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+.login {
+  width: 100%; /* 添加此行 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+}
 </style>
