eaf652015906ea7d4251759bfd101b098cc8d78c
qmtdlt <qmtdlt@gmail.com>
2025-03-07 19:23:50 +0800

    book

---
+++ api/src/QmtdltTools/QmtdltTools.Domain/Dtos/BookMainDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QmtdltTools.Domain.Dtos
+{
+    public class BookMainDto
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+        public byte[]? CoverImage { get; set; }
+        public byte[] BookBin { get; set; }
+    }
+}
--- api/src/QmtdltTools/QmtdltTools.Service/Services/EpubManageService.cs
+++ api/src/QmtdltTools/QmtdltTools.Service/Services/EpubManageService.cs
@@ -1,5 +1,8 @@
 ﻿using HtmlAgilityPack;
+using Microsoft.EntityFrameworkCore;
+using QmtdltTools.Domain.Entitys;
 using QmtdltTools.Domain.Models;
+using QmtdltTools.EFCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,25 +15,62 @@ namespace QmtdltTools.Service.Services
 {
     public class EpubManageService : ITransientDependency
     {
-        public EpubManageService()
+        private readonly DC _dc;
+        public EpubManageService(DC dc)
         {
-
+            _dc = dc;
         }
 
-        public async Task<Response<bool>> UploadEpub(EpubBook book)
+        public async Task<Response<bool>> UploadEpub(byte[] buffer)
         {
-            int cnt = 0;
-            foreach (EpubLocalTextContentFile textContentFile in book.ReadingOrder)
+            using (var ms = new MemoryStream(buffer))
             {
-                PrintTextContentFile(textContentFile);
-                Console.WriteLine(cnt++);
+                EpubBook book = EpubReader.ReadBook(ms);
+                // 如果存在相同 book.Title 和 book.Author 的电子书，则不再插入
+                if (_dc.EBooks.Any(e => e.Title == book.Title && e.Author == book.Author))
+                {
+                    return new Response<bool>
+                    {
+                        code = 1,
+                        message = "电子书已存在"
+                    };
+                }
+                else
+                {
+                    EBookMain eBookMain = new EBookMain
+                    {
+                        Title = book.Title,
+                        Author = book.Author,
+                        CoverImage = book.CoverImage,
+                        BookBin = buffer
+                    };
+                    _dc.EBooks.Add(eBookMain);
+                    await _dc.SaveChangesAsync();
+                }
             }
             return new Response<bool>
             {
                 data = true
             };
+            //_dc.
+
+            //int cnt = 0;
+            //foreach (EpubLocalTextContentFile textContentFile in book.ReadingOrder)
+            //{
+            //    PrintTextContentFile(textContentFile);
+            //    Console.WriteLine(cnt++);
+            //}
+            //return new Response<bool>
+            //{
+            //    data = true
+            //};
         }
 
+        public async Task<EBookMain?> GetBookById(Guid id)
+        {
+            var book = await _dc.EBooks.Where(t=>t.Id == id).FirstOrDefaultAsync();
+            return book;
+        }
         private static void PrintTextContentFile(EpubLocalTextContentFile textContentFile)
         {
             HtmlDocument htmlDocument = new();
