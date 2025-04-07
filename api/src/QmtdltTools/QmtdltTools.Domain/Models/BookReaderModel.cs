fc4c0cacd647cad3efb920ceec1b5ec0c8d73289
qmtdlt <qmtdlt@gmail.com>
2025-04-07 18:50:30 +0800

    自动读书

---
+++ api/src/QmtdltTools/QmtdltTools.Domain/Models/BookReaderModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VersOne.Epub;
+
+namespace QmtdltTools.Domain.Models
+{
+    public class BookReaderModel
+    {
+        public int position { get; set; }           // 位置
+        public EpubBook ebook { get; set; }         // 电子书对象
+    }
+
+    public class UIReadInfo
+    {
+        public string text { get; set; }
+        public byte[] buffer { get; set; }
+        public int position { get; set; }
+    }
+}
--- api/src/QmtdltTools/QmtdltTools/Hubs/BookContentHub.cs
+++ api/src/QmtdltTools/QmtdltTools/Hubs/BookContentHub.cs
@@ -3,6 +3,7 @@ using System.Formats.Tar;
 using System.Net;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.SignalR;
+using QmtdltTools.Domain.Models;
 using QmtdltTools.Service.Services;
 using QmtdltTools.Service.Utils;
 using VersOne.Epub;
@@ -33,28 +34,33 @@ public class BookContentHub:AbpHub
         return base.OnDisconnectedAsync(exception);
     }
 
-    static ConcurrentDictionary<Guid,int> bookReadPosition = new ConcurrentDictionary<Guid,int>();
-    static ConcurrentDictionary<string,bool> connectionStatusCache = new ConcurrentDictionary<string,bool>();
-    public async Task StartReadTask(Guid bookId)
+    public async Task InitCache(Guid bookId)
     {
         var connectionId = Context.ConnectionId;            // connectionId
 
         var book = await _epubManageService.GetBookById(bookId);            // 查询数据库，获取 book 对象
-        if(book == null)
+        if (book == null)
         {
             await Clients.All.SendAsync("onShowErrMsg", "未找到Id对应的电子书");
             return;
         }
-        if(File.Exists(book.BookPath))                                      // 检查电子书文件是否存在
+        if (File.Exists(book.BookPath))                                      // 检查电子书文件是否存在
         {
-            var ebook = EpubHelper.GetEbook(book.BookPath,out string message);  // 将磁盘上的电子书加载到内存中
+            var ebook = EpubHelper.GetEbook(book.BookPath, out string message);  // 将磁盘上的电子书加载到内存中
             if (ebook == null)
             {
                 await Clients.All.SendAsync("onShowErrMsg", message);           // 前端显示错误信息
             }
             else
             {
-                _ = StartReadingTask(connectionId, ebook, bookId);        // 启动阅读任务
+                int count = ebook.ReadingOrder.Count;                                       // 获取电子书的章节数
+                bool success = bookReadingCache.TryGetValue(bookId, out var bookInfo);
+                if (!success)
+                {
+                    bookInfo = new BookReaderModel { ebook = ebook, position = 0 };
+                    bookReadingCache.AddOrUpdate(bookId, bookInfo, (bookId, old) => { return bookInfo; });
+                }
+                await Clients.All.SendAsync("onSetBookPosition", bookInfo.position); // 通知前端初始化缓存
             }
         }
         else
@@ -63,19 +69,14 @@ public class BookContentHub:AbpHub
         }
     }
 
-    async Task StartReadingTask(string connectionId, EpubBook ebook,Guid bookId)
+    public async Task Read(Guid bookId,int position)
     {
-        int count = ebook.ReadingOrder.Count;                                       // 获取电子书的章节数
-        bool success = bookReadPosition.TryGetValue(bookId, out int position);
-        if (!success)
+        var connectionId = Context.ConnectionId;            // connectionId
+        bookReadingCache[bookId].position = position;       // 更新缓存中的位置
+
+        if (bookReadingCache[bookId].position < bookReadingCache[bookId].ebook.ReadingOrder.Count && connectionStatusCache[connectionId])
         {
-            position = 0;
-            // 更新 bookReadPosition 字典
-            bookReadPosition.TryAdd(bookId, position);
-        }
-        while (position < count && connectionStatusCache[connectionId])
-        {
-            var item = ebook.ReadingOrder[position];
+            var item = bookReadingCache[bookId].ebook.ReadingOrder[bookReadingCache[bookId].position];
 
             if (!string.IsNullOrEmpty(item.Content))
             {
@@ -84,28 +85,36 @@ public class BookContentHub:AbpHub
                 // 可进一步对文本进行整理，如去除多余空格
                 plainText = Regex.Replace(plainText, "\\s+", " ").Trim();
 
+                if(plainText.IsNullOrEmpty())
+                {
+                    bookReadingCache[bookId].position += 1;
+                    await Clients.All.SendAsync("onSetBookPosition", bookReadingCache[bookId].position); // 通知前端初始化缓存
+                    return;
+                }
+
                 var buffer = EpubHelper.GetSpeakStream(plainText);
 
-                bookIsReading.AddOrUpdate(bookId, true, (bookId, old) => { return true; });
-
-                await Clients.All.SendAsync("onPlayVoiceBuffer", buffer);                   // send audio buffer, this will be base64 string on client side
-                await Clients.All.SendAsync("onShowReadingText", plainText);                // send text
-                                                                                            // 通知前端 dowork
-                while (bookIsReading[bookId])
+                try
                 {
-                    await Task.Delay(50);
+                    await Clients.All.SendAsync("UIReadInfo", new UIReadInfo
+                    {
+                        buffer = buffer,
+                        text = plainText,
+                        position = bookReadingCache[bookId].position
+                    });                   // send audio buffer, this will be base64 string on client side
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
                 }
             }
-
-            position++;
-            bookReadPosition.TryUpdate(bookId, position, position - 1); // 更新字典中的位置
         }
     }
-    static ConcurrentDictionary<Guid, bool> bookIsReading = new ConcurrentDictionary<Guid, bool>();
-    public void bookGoNext(Guid bookId)
-    {
-        bookIsReading[bookId] = false;
-    }
+
+    static ConcurrentDictionary<Guid, BookReaderModel> bookReadingCache = new ConcurrentDictionary<Guid, BookReaderModel>();
+    static ConcurrentDictionary<string,bool> connectionStatusCache = new ConcurrentDictionary<string,bool>();
+    
     public async Task StopReadTask()
     {
         await Clients.All.SendAsync("onStopReadTask");
