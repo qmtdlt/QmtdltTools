66b2f36bd9780aeedaf96635b89db8a4752ac946
qmtdlt <qmtdlt@gmail.com>
2025-04-15 21:51:39 +0800

    db migration project

---
+++ api/src/QmtdltTools/QmtdltTools.DbMigration/DbMigrationHostedService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using QmtdltTools.EFCore;
+using Volo.Abp;
+
+namespace QmtdltTools.DbMigration;
+
+public class DbMigrationHostedService : IHostedService
+{
+    DC _dc;
+    public DbMigrationHostedService(DC dc)
+    {
+        _dc = dc;
+        _= Start();
+    }
+
+    public async Task Start()
+    {
+        var res = _dc.SysUsers.AsQueryable().ToList();
+        Console.WriteLine(res.Count);
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
