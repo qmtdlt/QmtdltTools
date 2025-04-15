d71845ba1d12576f775a2480dbab41e1dd9fdac6
qmtdlt <qmtdlt@gmail.com>
2025-04-15 21:51:39 +0800

    db migration project

---
+++ api/src/QmtdltTools/QmtdltTools.DbMigration/DbMigrationModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using QmtdltTools.EFCore;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace QmtdltTools.DbMigration;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(QmtdltToolsEFCoreModule)
+)]
+public class DbMigrationModule : AbpModule
+{
+    public override Task OnApplicationInitializationAsync(ApplicationInitializationContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
