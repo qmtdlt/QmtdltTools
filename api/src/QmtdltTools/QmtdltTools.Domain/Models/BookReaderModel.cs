460fb07fc497a8f9af772fe4be44615c4d2d942b
qmtdlt <qmtdlt@gmail.com>
2025-04-16 17:24:13 +0800

    read

--- api/src/QmtdltTools/QmtdltTools.Domain/Models/BookReaderModel.cs
+++ api/src/QmtdltTools/QmtdltTools.Domain/Models/BookReaderModel.cs
@@ -10,15 +10,67 @@ namespace QmtdltTools.Domain.Models
 {
     public class BookReaderModel
     {
-        public int position { get; set; }           // 位置
-        public EpubBook ebook { get; set; }         // 电子书对象
+        public List<MyPragraph> plist { get; set; }         // 电子书对象
+        public ReadPosition position { get; set; }           // 位置
+        public bool PositionInbook()
+        {
+            if(position.PragraphIndex < plist.Count && position.SentenceIndex < plist[position.PragraphIndex].Sentences.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool PositionNext()
+        {
+            if (position.SentenceIndex + 1 < plist[position.PragraphIndex].Sentences.Count)
+            {
+                position.SentenceIndex++;
+                return true;
+            }
+            else
+            {
+                if (position.PragraphIndex + 1 < plist.Count)
+                {
+                    position.PragraphIndex++;
+                    position.SentenceIndex = 0;
+                    return true;
+                }
+                else
+                {
+                    // isEnd
+                    return false;
+                }
+            }
+        }
+        public UIReadInfo GetPostionUIReadInfo()
+        {
+            if(PositionInbook())
+            {
+                return new UIReadInfo() { 
+                    full_pragraph_text = plist[position.PragraphIndex].PragraphText, 
+                    speaking_text = plist[position.PragraphIndex].Sentences[position.SentenceIndex], 
+                    speaking_buffer = new byte[0],
+                    position = position 
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
         public ConcurrentQueue<UIReadInfo> readQueue { get; set; } = new ConcurrentQueue<UIReadInfo>(); // 队列
     }
 
     public class UIReadInfo
     {
-        public string text { get; set; }
-        public byte[] buffer { get; set; }
-        public int position { get; set; }
+        public string full_pragraph_text { get; set; }
+        public string speaking_text { get; set; }
+        public byte[] speaking_buffer { get; set; }
+        public ReadPosition position { get; set; }
+    }
+    public class ReadPosition
+    {
+        public int PragraphIndex { get; set; }
+        public int SentenceIndex { get; set; }
     }
 }
--- api/src/QmtdltTools/QmtdltTools/Hubs/BookContentHub.cs
+++ api/src/QmtdltTools/QmtdltTools/Hubs/BookContentHub.cs
@@ -1,5 +1,6 @@
 using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
+using Autofac.Core.Resolving.Middleware;
 using Microsoft.AspNetCore.SignalR;
 using QmtdltTools.Domain.Models;
 using QmtdltTools.Service.Services;
@@ -11,6 +12,12 @@ namespace QmtdltTools.Hubs;
 //[Authorize]
 public class BookContentHub:AbpHub
 {
+    // static dict for all users
+    // 
+    static ConcurrentDictionary<Guid, BookReaderModel> bookReadingCache = new ConcurrentDictionary<Guid, BookReaderModel>();
+    // connction is alive
+    static ConcurrentDictionary<string, bool> connectionStatusCache = new ConcurrentDictionary<string, bool>();
+    // DI
     private readonly EpubManageService _epubManageService;
     public BookContentHub(EpubManageService epubManageService)
     {
@@ -19,43 +26,45 @@ public class BookContentHub:AbpHub
 
     public override Task OnConnectedAsync()
     {
-        Console.WriteLine("on connected");
         var connectionId = Context.ConnectionId;
-        connectionStatusCache.AddOrUpdate(connectionId, true, (connectionId, old) => { return true; });
+        connectionStatusCache.AddOrUpdate(connectionId, true, (connectionId, old) => { return true; });         // set connection alive
         return base.OnConnectedAsync();
     }
     public override Task OnDisconnectedAsync(Exception? exception)
     {
-        Console.WriteLine("on disconnected");
         var connectionId = Context.ConnectionId;
-        connectionStatusCache.AddOrUpdate(connectionId, false, (connectionId, old) => { return false; });
+        connectionStatusCache.AddOrUpdate(connectionId, false, (connectionId, old) => { return false; });       // set connection dead
         return base.OnDisconnectedAsync(exception);
     }
 
     public async Task InitCache(Guid bookId)
     {
-        var connectionId = Context.ConnectionId;            // connectionId
+        var connectionId = Context.ConnectionId;                            // connectionId
 
-        var book = await _epubManageService.GetBookById(bookId);            // 
+        var book = await _epubManageService.GetBookById(bookId);            // read book info from database
         if (book == null)
         {
-            await Clients.All.SendAsync("onShowErrMsg", "book not found");         //
+            await Clients.All.SendAsync("onShowErrMsg", "book not found in database");         // 
             return;
         }
-        if (File.Exists(book.BookPath))                                      // 
+        if (File.Exists(book.BookPath))                                             // if file exists
         {
-            var ebook = EpubHelper.GetEbook(book.BookPath, out string message);  // get ebook
+            var ebook = EpubHelper.GetEbook(book.BookPath, out string message);     // get ebook
             if (ebook == null)
             {
-                await Clients.All.SendAsync("onShowErrMsg", message);           // show err
+                await Clients.All.SendAsync("onShowErrMsg", message);               // show err
             }
             else
             {
-                int count = ebook.ReadingOrder.Count;                                       // 
+                List<MyPragraph> plist = EpubHelper.PrepareAllPragraphs(ebook);     // analyse book and get all pragraphs
+                // read book reading progress from redis or database
+                                                      
                 bool success = bookReadingCache.TryGetValue(bookId, out var bookInfo);
                 if (!success)
                 {
-                    bookInfo = new BookReaderModel { ebook = ebook, position = 0 };
+                    bookInfo = new BookReaderModel { plist = plist, position = new ReadPosition {
+                        PragraphIndex = 0, SentenceIndex = 0 
+                    } };
                     bookReadingCache.AddOrUpdate(bookId, bookInfo, (bookId, old) => { return bookInfo; });
                 }
                 await Clients.All.SendAsync("onSetBookPosition", bookInfo.position); // 
@@ -66,39 +75,35 @@ public class BookContentHub:AbpHub
             await Clients.All.SendAsync("onShowErrMsg", "epub file doesn't exist"); // 
         }
     }
-
-    public async Task Read(Guid bookId,int position)
+    public async Task ReadNext(Guid bookId)
+    {
+        bookReadingCache[bookId].PositionNext();
+        await Read(bookId);
+    }
+    public async Task Read(Guid bookId)
     {
         var connectionId = Context.ConnectionId;            // connectionId
-        bookReadingCache[bookId].position = position;       // pos
 
-        if (bookReadingCache[bookId].position < bookReadingCache[bookId].ebook.ReadingOrder.Count && connectionStatusCache[connectionId])
+        if (bookReadingCache[bookId].PositionInbook() && connectionStatusCache[connectionId])
         {
             bool success = bookReadingCache[bookId].readQueue.TryDequeue(out UIReadInfo uiReadInfo);
             if (success)
             {
-                _ = Clients.All.SendAsync("UIReadInfo", uiReadInfo);
+                _ = Clients.All.SendAsync("UIReadInfo", uiReadInfo);            // call client speak
             }
             else
             {
-                success = MakeQueueData(bookId);
-                
-                if(!success)
-                {
-                    bookReadingCache[bookId].position += 1;
-                    await Clients.All.SendAsync("onSetBookPosition", bookReadingCache[bookId].position); 
-                    return;
-                }
-                else
-                {
-                    success = bookReadingCache[bookId].readQueue.TryDequeue(out UIReadInfo uiReadInfo2);
-                    if(success)
-                        _ = Clients.All.SendAsync("UIReadInfo", uiReadInfo2);
-                }
+                success = MakeQueueData(bookId);            // TODO
             }
-
-            bookReadingCache[bookId].position += 1; // pos
-            MakeQueueData(bookId);
+            if(bookReadingCache[bookId].PositionNext())
+            {
+                MakeQueueData(bookId);
+            }
+            else
+            {
+                // is End
+                Console.WriteLine("isEnd");
+            }
         }
     }
     public async Task StopReadTask()
@@ -108,44 +113,27 @@ public class BookContentHub:AbpHub
 
     bool MakeQueueData(Guid bookId)
     {
-        bool success = GetCurContent(bookId, out byte[] buffer,out string plainText);
+        bool success = GetCurContent(bookId, out UIReadInfo uiReadInfo);
         if (success)
         {
-            bookReadingCache[bookId].readQueue.Enqueue(new UIReadInfo
-            {
-                buffer = buffer,
-                text = plainText,
-                position = bookReadingCache[bookId].position
-            });
-        }
-        return success;
-    }
-    bool GetCurContent(Guid bookId,out byte[] buffer,out string plainText)
-    {
-        buffer = null;
-        plainText = "";
-        Console.WriteLine("prepare position: " + bookReadingCache[bookId].position);
-        var item = bookReadingCache[bookId].ebook.ReadingOrder[bookReadingCache[bookId].position];
-        
-        if (!string.IsNullOrEmpty(item.Content))
-        {
-            // get plain text
-            plainText = Regex.Replace(item.Content, "<[^>]+>", " ");
-            // remove spaces
-            plainText = Regex.Replace(plainText, "\\s+", " ").Trim();
-
-            if(plainText.IsNullOrEmpty()) return false;
-            buffer = EpubHelper.GetSpeakStream(plainText);
+            bookReadingCache[bookId].readQueue.Enqueue(uiReadInfo);
             return true;
-        }
-        if(bookReadingCache[bookId].position + 1 < bookReadingCache[bookId].ebook.ReadingOrder.Count)
-        {
-            return false;
         }
         return false;
     }
-    
-    static ConcurrentDictionary<Guid, BookReaderModel> bookReadingCache = new ConcurrentDictionary<Guid, BookReaderModel>();
-    static ConcurrentDictionary<string,bool> connectionStatusCache = new ConcurrentDictionary<string,bool>();
-    
+    bool GetCurContent(Guid bookId,out UIReadInfo uiReadInfo)
+    {
+        uiReadInfo = new UIReadInfo();
+        
+        uiReadInfo = bookReadingCache[bookId].GetPostionUIReadInfo();
+        if(null == uiReadInfo) return false;
+
+        if (!string.IsNullOrEmpty(uiReadInfo.speaking_text))
+        {
+            if(uiReadInfo.speaking_text.IsNullOrEmpty()) return false;
+            uiReadInfo.speaking_buffer = EpubHelper.GetSpeakStream(uiReadInfo.speaking_text);
+            return true;
+        }
+        return false;
+    }
 }
