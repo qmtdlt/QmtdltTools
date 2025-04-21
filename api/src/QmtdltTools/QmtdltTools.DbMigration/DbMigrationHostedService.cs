63447832d0c83110f5d549ce2416de1ca60d1274
qmtdlt <qmtdlt@gmail.com>
2025-04-21 10:04:20 +0800

    备份及还原种子数据

--- api/src/QmtdltTools/QmtdltTools.DbMigration/DbMigrationHostedService.cs
+++ api/src/QmtdltTools/QmtdltTools.DbMigration/DbMigrationHostedService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using QmtdltTools.Domain.Entitys;
 using QmtdltTools.EFCore;
@@ -16,29 +20,76 @@ public class DbMigrationHostedService : IHostedService
     {
         _dc = dc;
     }
-
+    public async Task BakupData()
+    {
+        // 备份数据
+        System.Collections.Generic.List<SysUser> users = await _dc.SysUsers.ToListAsync();
+        System.Collections.Generic.List<EBookMain> ebooks = await _dc.EBooks.ToListAsync();
+        System.Collections.Generic.List<ListenWriteRecord> listenwrite = await _dc.ListenWriteRecords.ToListAsync();
+        System.Collections.Generic.List<VocabularyRecord> vocabulary = await _dc.VocabularyRecords.ToListAsync();
+        // 将数据备份到文件
+        File.WriteAllText("users.json", System.Text.Json.JsonSerializer.Serialize(users));
+        File.WriteAllText("ebooks.json", System.Text.Json.JsonSerializer.Serialize(ebooks));
+        File.WriteAllText("listenwrite.json", System.Text.Json.JsonSerializer.Serialize(listenwrite));
+        File.WriteAllText("vocabulary.json", System.Text.Json.JsonSerializer.Serialize(vocabulary));
+    }
     public async Task Start()
     {
         await _dc.Database.EnsureCreatedAsync();
-
-        if (!_dc.SysUsers.Any())
+        // 加载数据
+        Console.WriteLine("1. Do you want to backup data to json file? input y for yes.");
+        var input = Console.ReadLine();
+        if (input == "y")
         {
-            // 添加种子数据
-            _dc.SysUsers.AddRange(new[]
+            await BakupData();
+        }
+        Console.WriteLine("2. Do you want to insert the data from json file to database? input y for yes.");
+        input = Console.ReadLine();
+        if (input == "y")
+        {
+            try
             {
-                new SysUser
-                {
-                    Name = "qmtdlt",
-                    Code = "qmtdlt",
-                    Email = "qmtdlt@gmail.com",
-                    PasswordHash = "12000asd", // 替换为实际的哈希密码
-                    PhoneNumber = "13679112984",
-                    IsActive = true
-                }
-            });
-
-            // 保存更改
-            await _dc.SaveChangesAsync();
+                var list = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.List<SysUser>>(File.ReadAllText("users.json"));
+                await _dc.SysUsers.AddRangeAsync(list);
+                await _dc.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // ebook
+            try
+            {
+                var list = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.List<EBookMain>>(File.ReadAllText("ebooks.json"));
+                await _dc.EBooks.AddRangeAsync(list);
+                await _dc.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // listenwrite
+            try
+            {
+                var list = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.List<ListenWriteRecord>>(File.ReadAllText("listenwrite.json"));
+                await _dc.ListenWriteRecords.AddRangeAsync(list);
+                await _dc.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // vocabulary
+            try
+            {
+                var list = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.List<VocabularyRecord>>(File.ReadAllText("vocabulary.json"));
+                await _dc.VocabularyRecords.AddRangeAsync(list);
+                await _dc.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 
