7e9d810abc1f695df91c89c5e53f6e2383e22039
qmtdlt <qmtdlt@gmail.com>
2025-04-25 00:34:28 +0800

    样式修改

--- web/qmtdlt_tools_vue/src/router/index.ts
+++ web/qmtdlt_tools_vue/src/router/index.ts
@@ -41,7 +41,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/PlayGround.vue'),
+      component: () => import('../views/BookReader.vue'),
     },
     {
       path: '/mobilebookview',
