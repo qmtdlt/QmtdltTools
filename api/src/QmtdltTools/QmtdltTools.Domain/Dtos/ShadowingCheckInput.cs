6f4f55e040ae504d32cab64ed2753811d995279b
qmtdlt <qmtdlt@gmail.com>
2025-05-16 18:43:16 +0800

    11111

---
+++ api/src/QmtdltTools/QmtdltTools.Domain/Dtos/ShadowingCheckInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QmtdltTools.Domain.Dtos
+{
+    public class ShadowingCheckInput
+    {
+        public string audioBase64 { get; set; }
+    }
+}
