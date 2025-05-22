6fb90393eac74107086af87829282c2facb12c74
qmtdlt <qmtdlt@gmail.com>
2025-05-22 15:11:34 +0800

    优化翻译弹窗

--- web/qmtdlt_tools_vue/src/views/TranslateView.vue
+++ web/qmtdlt_tools_vue/src/views/TranslateView.vue
@@ -33,6 +33,27 @@
             </el-row>
         </div>
     </el-dialog>
+    <el-dialog v-model="showSelectDialog" width="320px" :show-close="false" center>
+        <div style="text-align:center;">
+            <el-row justify="center" style="margin-bottom:20px;">
+                <el-button @click="copyPhaseText" type="primary" plain circle>
+                    <el-icon size="large">
+                        <DocumentCopy />
+                    </el-icon>
+                </el-button>
+                <el-button @click="confirmTrans" type="success" plain circle>
+                    <el-icon size="large">
+                        <IconTranslate />
+                    </el-icon>
+                </el-button>
+                <el-button @click="cancelSelect" type="danger" plain circle>
+                    <el-icon size="large">
+                        <Close />
+                    </el-icon>
+                </el-button>
+            </el-row><h2 style="margin: 20px 0 10px 0;">{{ selectDialogText }}</h2>
+        </div>
+    </el-dialog>
 </template>
 
 <script setup lang="ts">
@@ -40,13 +61,29 @@ import { ref, onMounted, watch } from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus';
 import { startPlayBase64Audio, stopPlayBase64Audio, cleanupAudio } from '../utils/audioplay';
 import request from '@/utils/request';
-import { Headset, VideoPause, CaretRight, Close, ArrowLeft, ArrowRight } from '@element-plus/icons-vue'
-
+import { Headset, VideoPause, DocumentCopy, Close, ArrowLeft, ArrowRight } from '@element-plus/icons-vue'
+import IconTranslate from '@/components/icons/IconTranslate.vue'
 const transSource = ref(''); // Store the selected text for translation
 const showTransDialog = ref(false) // 控制翻译弹窗显示
 const translating = ref(false); // 拖拽到右侧区域的文本
 const transResult = ref<VocabularyRecord>({} as VocabularyRecord); // Store translation result pronunciation is base64 string
+const showSelectDialog = ref(false)
+const selectDialogText = ref('')
 
+const copyPhaseText = async () => {
+    await navigator.clipboard.writeText(selectDialogText.value)
+    ElMessage.success('已复制到剪贴板')
+    showSelectDialog.value = false
+}
+
+const confirmTrans = async () => {
+    showSelectDialog.value = false
+    await realHandlePhaseSelect(selectDialogText.value)
+}
+
+const cancelSelect = () => {
+    showSelectDialog.value = false
+}
 
 interface VocabularyRecord {
     id: string
@@ -64,55 +101,35 @@ interface VocabularyRecord {
 }
 
 const handlePhaseSelect = async (phaseText: string) => {
+    selectDialogText.value = phaseText
+    showSelectDialog.value = true
+
+}
+const realHandlePhaseSelect = async (phaseText: string) => {
     if (translating.value) {
         console.log("Translation already in progress. Ignoring phase select.");
         return; // Prevent multiple translation requests
     }
-
     try {
-
-        await ElMessageBox.confirm(
-            `是否处理新单词: "${phaseText}"?`,
-            '提示',
-            {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'info',
-                // Disable closing on clicks outside or ESC key
-                closeOnClickModal: false,
-                closeOnPressEscape: false
-            }
-        );
         translating.value = true; // Set loading immediately
-        showTransDialog.value = true; // Hide dialog if it was open
+        showTransDialog.value = true; // Show dialog if it was open
         let res = await request.get<VocabularyRecord>(
             '/api/Vocabulary/Trans',
             {
-                params: {
-                    word: phaseText
-                },
+                params: { word: phaseText },
             }
         );
-        // User clicked Confirm
-        console.log("trans result.", res);
-
-        transSource.value = phaseText; // Store the selected text for translation
-
+        transSource.value = phaseText;
         translating.value = false;
-        transResult.value = res; // Store the translation result
-        console.log("Received translation. Playing audio.");
-        // Play translation voice - onEnded is not needed here
+        transResult.value = res;
         startPlayBase64Audio(transResult.value.pronunciation, () => {
             console.log("Translation audio playback finished.");
         });
-
     } catch (e: any) {
-        // User clicked Cancel or an error occurred in the confirm box
         console.log("User cancelled translation or confirm failed:", e);
         ElMessage.info('已取消');
-        translating.value = false; // Hide loading
-        showTransDialog.value = false; // Ensure dialog is closed if it opened prematurely
-        // isFirstTime.value = true; // Reset for the next selection attempt
+        translating.value = false;
+        showTransDialog.value = false;
     }
 }
 
@@ -126,6 +143,6 @@ const playTransVoice = (voiceBuffer: string) => {
     }
 }
 defineExpose({
-  handlePhaseSelect
+    handlePhaseSelect
 });
 </script>
