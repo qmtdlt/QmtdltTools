e1079d55e5b04b9761f489bdaa3bddd7a99d4d01
qmtdlt <qmtdlt@gmail.com>
2025-07-08 10:02:11 +0800

    maui

---
+++ api/src/QmtdltTools/QmtdltTools.MAUI.Blazor/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace QmtdltTools.MAUI.Blazor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "QmtdltTools.MAUI.Blazor" };
+        }
+    }
+}
---
+++ api/src/QmtdltTools/QmtdltTools.MAUI.Blazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QmtdltTools.MAUI.Blazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
